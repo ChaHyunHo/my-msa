338678bc84d04c9870489cd36af751281e9a1e74
chahyeonho <chamym@naver.com>
2025-08-09 23:54:22 +0900

    product update

--- msa-product/src/main/java/me/chahyunho/msaproduct/controller/ProductController.java
+++ msa-product/src/main/java/me/chahyunho/msaproduct/controller/ProductController.java
@@ -1,7 +1,5 @@
 package me.chahyunho.msaproduct.controller;
 
-import io.swagger.annotations.Api;
-import io.swagger.annotations.ApiOperation;
 import me.chahyunho.msaproduct.model.Product;
 import me.chahyunho.msaproduct.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,20 +13,17 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/product")
-@Api(value = "Product Service API")
 public class ProductController {
 
     @Autowired
     private ProductService productService;
 
     @PostMapping
-    @ApiOperation(value = "Create a new product")
     public ResponseEntity<Product> createProduct(@RequestBody Product product) {
         return ResponseEntity.ok(productService.createProduct(product));
     }
 
     @GetMapping("/{id}")
-    @ApiOperation(value = "Get product by ID")
     public ResponseEntity<Product> getProductById(@PathVariable Long id) {
         return ResponseEntity.ok(productService.getProductById(id));
     }
